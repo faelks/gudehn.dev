af8d7e7cc50a9fd6202b78167a4f786e73b764b2
Felix Gudéhn <felix.gudehn@gmail.com>
2020-04-06 14:03:50 +1000

    tutorial commit 1

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Link to="/">Home</Link>
-    <Header headerText="Contact" />
-    <p>Send us a message!</p>
+    <Header headerText="About Gatsby" />
+    <p>Such wow. Very React.</p>
   </div>
 )
