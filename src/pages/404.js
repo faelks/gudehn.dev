5f18c2832e9cb7c8ab0c86269c167a3fc125c889
Felix Gudéhn <felix.gudehn@gmail.com>
2020-04-09 14:40:26 +1000

    Add basic 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  Container,
+  Header,
+  Layout,
+  PostsTable,
+  Stack,
+  SubHeading,
+} from "../components";
+
+export default () => (
+  <Layout>
+    <Header title="404 Not Found"></Header>
+    <Container padding="medium">
+      <Stack yPadding="medium">
+        <h4>
+          There was no page found here, try one of these blog posts instead!
+        </h4>
+        <PostsTable />
+      </Stack>
+    </Container>
+  </Layout>
+);
